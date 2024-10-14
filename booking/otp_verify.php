bc6e183c60e762cc602b5dd9f2c8ee3bcb11ca2e
capstonehotel <ungonkathleen@gmail.com>
2024-10-14 10:50:42 -0500

    commit

--- booking/otp_verify.php
+++ booking/otp_verify.php
@@ -1,24 +1,22 @@
 <?php
 session_start();
+
 if (isset($_POST['otp'])) {
-    $userOtp = $_POST['otp'];
-    $email = $_POST['email']; // Get the email address from the form
-    //echo '<script>alert(:);</script>';
+    $userOtp = filter_var($_POST['otp'], FILTER_SANITIZE_NUMBER_INT);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
     // Verify that the OTP code was sent to the correct email address
     if (isset($_SESSION['otp_email']) && $_SESSION['otp_email'] == $email) {
-
-        //echo '<script>alert("'.$_SESSION['otp'].'");</script>';
         // Verify that the OTP code is valid
         if (preg_match('/^[0-9]{6}$/', $userOtp)) {
             // User-input OTP is a valid OTP code
             if ($userOtp == $_SESSION['otp']) {
-                    // OTP is valid, return success message
-                    echo 'valid';
-                } else {
-                    // OTP is invalid, return error message
-                    echo 'invalid';
-                }
-           
+                // OTP is valid, return success message
+                echo 'valid';
+            } else {
+                // OTP is invalid, return error message
+                echo 'invalid';
+            }
         } else {
             echo 'Invalid OTP code'; // Handle the case where the user-input OTP is not a valid OTP code
         }
