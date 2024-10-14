4798eb855e1ad1445334ce62fba6525c7b9a761f
capstonehotel <ungonkathleen@gmail.com>
2024-10-14 18:37:01 -0500

    c

--- login.php
+++ login.php
@@ -1,26 +1,29 @@
 <?php
-require_once("initialize.php"); 
+ require_once ("initialize.php"); 
 
-if (isset($_POST['gsubmit'])) {
-    $email = trim($_POST['username']);
-    $upass  = trim($_POST['pass']);
-    $h_upass = sha1($upass);
-}
 
-if ($email == '' OR $upass == '') { 
-    message("Invalid Username and Password!", "error");
-    redirect("https://mcchmhotelreservation.com/index.php?page=6");
-} else {   
-    $guest = new Guest();
-    $res = $guest::guest_login($email, $h_upass);
 
-    if ($res == true) {
-        // Send OTP email logic can be included here
-        $_SESSION['email'] = $email; // Store the email for OTP sending
-        redirect("sendOTP.php"); // Redirect to send OTP
-    } else {
-        message("Invalid Username and Password! Please contact administrator", "error");
-        redirect("https://mcchmhotelreservation.com/booking/index.php?view=logininfo");
-    }
-}
+ if(isset($_POST['gsubmit'])){
+
+  $email = trim($_POST['username']);
+  $upass  = trim($_POST['pass']);
+  $h_upass = sha1($upass);
+  }
+   if ($email == '' OR $upass == '') { 
+      message("Invalid Username and Password!", "error");
+       redirect("https://mcchmhotelreservation.com/index.php?page=6");
+         
+    } else {   
+        $guest = new Guest();
+        $res = $guest::guest_login($email,$h_upass);
+
+        if ($res==true){
+           redirect("https://mcchmhotelreservation.com/booking/index.php?view=payment");
+        }else{
+             message("Invalid Username and Password! Please contact administrator", "error");
+             redirect("https://mcchmhotelreservation.com/booking/index.php?view=logininfo");
+        }
+ 
+ }
+ 
 ?>
