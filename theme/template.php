ac53da3102cdd83873e51e8821a098fe0bf64c30
capstonehotel <ungonkathleen@gmail.com>
2024-10-13 19:58:42 -0500

    commit

--- theme/template.php
+++ theme/template.php
@@ -17,7 +17,8 @@ $_SESSION['to']  = $_POST['to'];
     <meta name="generator" content="Hugo 0.115.4">
     <link rel="stylesheet" type="text/css" href="https://mcchmhotelreservation.com/fonts/css/font-awesome.min.css" />
     <title><?php echo isset($title) ? $title . ' | HM Hotel' : 'HM mini Hotel' ; ?></title>
-
+<!-- For .png format -->
+<link rel="icon" href="../logo.jpg" type="image/png">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/navbars-offcanvas/">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
