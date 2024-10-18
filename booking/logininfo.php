d69217b588c4b7957503ad0465e23ae9f237410c
capstonehotel <ungonkathleen@gmail.com>
2024-10-18 11:50:19 -0500

    c

--- booking/logininfo.php
+++ booking/logininfo.php
@@ -1,156 +1,194 @@
 <?php 
-// session_start(); // Start session management
-require_once("sendOTP.php");
-if (!isset($_SESSION['monbela_cart'])) {
-    redirect('https://mcchmhotelreservation.com/index.php');
-}
-?>
 
+if (!isset($_SESSION['monbela_cart'])) {
+  # code...
+  redirect('https://mcchmhotelreservation.com/index.php');
+}
+
+
+
+ ?>
 <div class="card rounded" style="padding: 10px;">
-    <div class="pagetitle">   
-        <h1>Your Booking Cart</h1> 
+  <div  class="pagetitle">   
+        <h1  >Your Booking Cart 
+        </h1> 
     </div>
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb" style="margin-top: 10px;">
-            <li class="breadcrumb-item"><a href="https://mcchmhotelreservation.com/index.php">Home</a></li>
-            <li class="breadcrumb-item"><a href="https://mcchmhotelreservation.com/booking/">Booking Cart</a></li>
-            <li class="breadcrumb-item active">Verify Accounts</li>
-        </ol>
+    <nav aria-label="breadcrumb" >
+      <ol class="breadcrumb" style="margin-top: 10px;">
+        <li class="breadcrumb-item"><a href="https://mcchmhotelreservation.com/index.php">Home</a></li>
+        <li class="breadcrumb-item "><a href="https://mcchmhotelreservation.com/booking/">Booking Cart</a></li>
+        <li class="breadcrumb-item active">Verify Accounts</li>
+      </ol>
     </nav>
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <nav>
-                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                        <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Login</button>
-                        <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Register</button>
-                    </div>
-                </nav>
-                <div class="tab-content" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
-                        <div class="col-md-12">
-                            <h4>Service Two</h4> 
-                            <?php echo logintab(); ?>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
-                        <?php require_once 'personalinfo.php'; ?>
-                    </div>
-                </div>
+      <div class="row">
+        <div class="col-md-12">
+          <nav>
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+              <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Login</button>
+              <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Register</button>
             </div>
+          </nav>
+          <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+              <div class="col-md-12">
+                <h4>Service Two</h4> 
+                    <?php echo logintab(); ?>
+              </div>
+            </div>
+            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+              <?php  require_once 'personalinfo.php'; ?>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+
 </div>
 
+
+
 <?php
-function logintab() {
+
+  function logintab(){
+
     ?>  
-    <div class="col-md-12">
-        <form id="loginForm" method="post">
-            <div class="form-group has-feedback">
-                <input type="text" class="form-control" name="username" placeholder="Username" required>
-                <span class="glyphicon glyphicon-user form-control-feedback"></span>
-            </div>
-            <div class="form-group has-feedback" style="margin-top: 10px;">
-                <input type="password" class="form-control" name="pass" placeholder="Password" required>
-                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-            </div>
-            <div class="row">
-                <div class="col-xs-8">
-                    <div class="checkbox icheck">
-                        <label>
-                            <input type="checkbox"> Remember Me
-                        </label>
+  <div class="col-md-12">
+    <form action="<?php echo "https://mcchmhotelreservation.com/login.php" ?>" method="post">
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" name="username" placeholder="Username">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback" style="margin-top: 10px;">
+        <input type="password" class="form-control" name="pass" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Remember Me
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" name="gsubmit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form> 
+</div>
+ 
+ 
+
+<?php
+  }
+
+function listofbooking(){
+
+
+
+$payable = 0;
+if (isset( $_SESSION['monbela_cart'])){ 
+$count_cart = count($_SESSION['monbela_cart']);
+
+?>
+      <!-- list -->
+<div class="row">
+<div class="col-md-4">
+
+     <div style="overflow:scroll;  height:300px;">
+
+
+<?php
+for ($i=0; $i < $count_cart  ; $i++) {  
+
+  $query = "SELECT * FROM `tblroom` r ,`tblaccomodation` a WHERE r.`ACCOMID`=a.`ACCOMID` AND ROOMID=" . $_SESSION['monbela_cart'][$i]['monbelaroomid'];
+   $mydb->setQuery($query);
+   $cur = $mydb->loadResultList(); 
+    foreach ($cur as $result) { 
+
+
+?>             
+      
+        <div class="row"> 
+          <div class="col-md-12">
+             <div class="panel panel-default">
+                <div class="panel-heading">
+                <!-- <h4>Payment</h4> -->
+                </div>
+                <div class="panel-body">
+
+                    <div class="col-md-12">
+                      <label>Room:</label><br/>
+                      <?php echo  $result->ROOM.' '. $result->ROOMDESC; ?>
                     </div>
-                </div>
-                <div class="col-xs-4">
-                    <button type="submit" name="gsubmit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-                </div>
-            </div>
-        </form> 
-    </div>
+                   
+                    <div class="col-md-6">
+                      <label>Checkedin:</label>
+                      <?php echo  date_format(date_create( $_SESSION['monbela_cart'][$i]['monbelacheckin']),"m/d/Y"); ?>
+                    </div> 
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                    <div class="col-md-6">
+                       <label>Checkedout:</label>
+                      <?php echo  date_format(date_create( $_SESSION['monbela_cart'][$i]['monbelacheckout']),"m/d/Y"); ?>
+                    </div>   
+                  
+ 
+                      <div class="col-md-12" style="float:left;border-top:1px solid #000;">
+                          <label>Summary</label> 
+                      </div>
+                
+                      <div style="float:right">  
 
-    <script>
-    $(document).ready(function() {
-        $('#loginForm').on('submit', function(e) {
-            e.preventDefault(); // Prevent default form submission
+                          <div class="col-md-12"  >
+                              <label>Price:</label>
+                            <?php echo  ' &#8369 '. $result->PRICE; ?>
+                         </div> 
 
-            $.ajax({
-                type: 'POST',
-                url: 'login.php', // URL of the login script
-                data: $(this).serialize(), // Serialize form data
-                success: function(response) {
-                    // Check the response from the server
-                    if (response.trim() === 'success') {
-                        // Store the username in the session for OTP verification
-                        const username = $('input[name="username"]').val();
-                        // Update session variable using AJAX or handle it server-side
-                        showOtpInput(); // Show the SweetAlert for OTP input
-                    } else {
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Login Failed',
-                            text: response, // Display the error message
-                        });
-                    }
-                },
-                error: function() {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Oops...',
-                        text: 'Something went wrong!',
-                    });
-                }
-            });
-        });
-    });
+                         <div class="col-md-12"  >
+                              <label>Days:</label>
+                            <?php echo   $_SESSION['monbela_cart'][$i]['monbeladay']; ?>
+                         </div> 
 
-    function showOtpInput() {
-        Swal.fire({
-            title: 'Enter OTP',
-            input: 'text',
-            inputPlaceholder: 'Enter OTP code',
-            showCancelButton: true,
-            confirmButtonText: 'Verify OTP',
-            footer: `Didn't receive a code? <a href="#" id="resend-otp-link">Resend</a>`,
-        }).then((result) => {
-            if (result.value) {
-                $.ajax({
-                    type: 'POST',
-                    url: 'otp_verify.php',
-                    data: {
-                        otp: result.value,
-                        email: $('input[name="username"]').val() // Use the input value
-                    },
-                    success: function(response) {
-                        if (response.trim() == 'valid') {
-                            Swal.fire({
-                                icon: 'success',
-                                title: 'OTP Verified!',
-                                text: 'Redirecting to payment...',
-                                timer: 3000,
-                                timerProgressBar: true,
-                                showConfirmButton: false,
-                                willClose: () => {
-                                    window.location.href = 'index.php?view=payment';
-                                }
-                            });
-                        } else {
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Invalid OTP!',
-                                text: 'Please try again.',
-                            }).then(() => {
-                                showOtpInput(); // Show OTP input again if invalid
-                            });
-                        }
-                    }
-                });
-            }
-        });
+                         <div class="col-md-12" >
+                              <label>Total:</label>
+                            <?php echo ' &#8369 '.   $_SESSION['monbela_cart'][$i]['monbelaroomprice']; ?>
+                         </div> 
+
+                      </div>    
+                      
+                 </div>
+
+                 <div class="panel-footer">
+                   
+                 </div>
+              </div>   
+
+          </div>
+        </div> 
+  <?php 
     }
-</script>
-<?php }?>
+
+                      $payable += $_SESSION['monbela_cart'][$i]['monbelaroomprice'] ;
+  }
+                      $_SESSION['pay'] = $payable;
+}
+?>
+      <div class="col-md-12" >
+      <div class="row">
+          <label style="float:left">Overall Price</label> <h2 style="float:right"> &#8369 <?php echo   $_SESSION['pay'] ;?></h2> 
+      </div>
+        </div>
+
+
+  </div>  
+    
+  </div>  
+</div>
+      <!-- end list -->
+    
+<!-- end content -->
+<!-- =========================================================================== -->
+<?php } ?>
