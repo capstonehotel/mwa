f718d851fa1432aca7ca9d5942b21d4565e95df4
capstonehotel <ungonkathleen@gmail.com>
2024-10-19 20:10:58 +0800

    v

--- theme/template.php
+++ theme/template.php
@@ -289,7 +289,19 @@ try {
     }
   </script>
 
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67139ff22480f5b4f58ffdb9/1iaiahjef';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
 
 
