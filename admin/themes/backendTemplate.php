eac48d278ac65bcc0bb5f5cfe210d30db73c78d3
capstonehotel <ungonkathleen@gmail.com>
2024-10-22 22:59:35 +0800

    f

--- admin/themes/backendTemplate.php
+++ admin/themes/backendTemplate.php
@@ -372,7 +372,7 @@ mysqli_close($conn);
                           
                             <!-- Message -->
                             <p style="margin: 0 0 2px 0;">
-                                <strong><?php echo $_SESSION['name'] . ' ' . $_SESSION['last']; ?></strong> has made a booking of 112jhgkhkghgkgjkhgkjhgkhjkghgkjhj
+                                <strong><?php echo $_SESSION['name'] . ' ' . $_SESSION['last']; ?></strong> has made a booking <?php echo $row['ROOM']; ?>
                             </p>
                             <p class="time" style="margin-bottom: 5px;">
                                 11/22/2002
