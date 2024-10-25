410e227f7209e2165e0421f5bddf324cf63b60c8
capstonehotel <ungonkathleen@gmail.com>
2024-10-24 22:19:06 -0500

    s

--- booking/payment.php
+++ booking/payment.php
@@ -361,10 +361,10 @@ $_SESSION['GUESTID'] =   $lastguest;
                     </div>
                     
                     <div class="col-md-12">
-    <label style="display: none;">Transaction Id:</label>
-    <span style="display: none;"><?php echo $_SESSION['confirmation']; ?></span>
-    <input type="hidden" name="realconfirmation" value="<?php echo $_SESSION['confirmation']; ?>" />
-    <input type="hidden" id="payment_status_input"  name="txtstatus">
+    <label >Transaction Id:</label>
+    <span name="realconfirmation"><?php echo $_SESSION['confirmation']; ?></span>
+    <!-- <input type="hidden" name="realconfirmation" value="<?php echo $_SESSION['confirmation']; ?>" />
+    <input type="hidden" id="payment_status_input"  name="txtstatus"> -->
 </div>
 <div class="col-md-12 col-sm-2">
         <label id="paymentLabel">Payment Method:</label>
