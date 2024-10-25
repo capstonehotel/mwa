5224bf4e32c6b1cffb7498e97892111a1f6e7262
capstonehotel <ungonkathleen@gmail.com>
2024-10-25 16:01:44 -0500

    s

--- admin/home.php
+++ admin/home.php
@@ -16,13 +16,12 @@ $query = "SELECT count(*) as 'Total' FROM `tblroom` WHERE ROOM != '' ";
  ?>
 <div class="col-xl-4 col-md-6 mb-4" >
 <div class="card board1 fill">
-    <div class="card border-left-primary shadow h-100 py-2">
-    
+    <div class="card shadow h-100 py-2">  
         <div class="card-body">
         <div class="dash-widget-header">
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
-                <div class="h5 mb-0 font-weight-bold text-gray-800" style="font-size: 24px;"><?php  echo  isset($result->Total) ? $result->Total  : 0;?></div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800" style="font-size: 30px;"><?php  echo  isset($result->Total) ? $result->Total  : 0;?></div>
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                         Rooms</div>
                     
