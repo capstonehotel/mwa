983d0ae68b1805af04d5fa697e4a3ef50354dae5
capstonehotel <ungonkathleen@gmail.com>
2024-10-25 17:13:58 -0500

    s

--- admin/home.php
+++ admin/home.php
@@ -192,6 +192,38 @@ foreach ($curya as $resultas) {
     </div>
 </div>
 <?php } ?>
+<?php 
+    $querysi = "SELECT count(*) as 'Total' FROM `tblreservation` WHERE STATUS= 'Cancelled'  ";
+                $mydb->setQuery($querysi);
+                $curya = $mydb->loadResultList();  
+                foreach ($curya as $resultas) { 
+   ?>
+
+
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card board1 fill">
+        <div class="card shadow h-100 py-2">
+            <div class="card-body">
+                <div class="dash-widget-header">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h5 mb-0 font-weight-bold text-gray-800" style="font-size: 30px;">
+                                ₱<?php echo isset($resultas->Total) ? number_format($resultas->Total, 2) : '0.00'; ?>
+                            </div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Total Invoice
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i data-feather="file-text" class="icon"></i> <!-- You can use a file or document icon -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php } ?>
 
 
 <br>
