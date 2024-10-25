d7935f6078cac56ea99ea0c6e20644a095f18269
capstonehotel <ungonkathleen@gmail.com>
2024-10-25 14:31:20 -0500

    s

--- admin/home.php
+++ admin/home.php
@@ -73,23 +73,29 @@ $query = "SELECT count(*) as 'Total' FROM `tblroom` WHERE ROOM != '' ";
         }
     </style>
     <div class="row">
-					<div class="col-xl-3 col-sm-6 col-12">
-						<div class="card board1 fill">
-							<div class="card-body">
-								<div class="dash-widget-header">
-									<div>
-										<h3 class="card_widget_header">236</h3>
-										<h6 class="text-muted">Total Booking</h6> </div>
-									<div class="ml-auto mt-md-3 mt-lg-0"> <span class="opacity-7 text-muted"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24" fill="none" stroke="#009688" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus">
-									<path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-									<circle cx="8.5" cy="7" r="4"></circle>
-									<line x1="20" y1="8" x2="20" y2="14"></line>
-									<line x1="23" y1="11" x2="17" y2="11"></line>
-									</svg></span> </div>
-								</div>
-							</div>
-						</div>
-					</div>
+    <div class="col-xl-3 col-sm-6 col-12">
+        <div class="card board1 fill">
+            <div class="card-body">
+                <div class="dash-widget-header">
+                    <div>
+                        <h3 class="card_widget_header">236</h3>
+                        <h6 class="text-muted">Total Booking</h6>
+                    </div>
+                    <div class="ml-auto mt-md-3 mt-lg-0">
+                        <span class="opacity-7 text-muted">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="#009688" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bed">
+                                <path d="M3 12h18v6H3z"></path>
+                                <path d="M3 12V6h18v6"></path>
+                                <path d="M6 6v6"></path>
+                                <path d="M18 6v6"></path>
+                            </svg>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 $query = "SELECT count(*) as 'Total' FROM `tblroom` WHERE ROOM != '' ";
                 $mydb->setQuery($query);
