6761acfe022cfff9fee17ff8fa0f40ec2fa7bb0c
capstonehotel <ungonkathleen@gmail.com>
2024-10-26 01:25:22 -0500

    e

--- admin/home.php
+++ admin/home.php
@@ -70,7 +70,7 @@ $mydb->setQuery($query2);
 $cur2 = $mydb->loadResultList();
 foreach ($cur2 as $result2) {
 ?>
-<div class="col-xl-2 col-md-2 mb-4">
+<div class="col-xl-3 col-md-2 mb-4">
     <div class="card board1 fill">
         <div class="card shadow h-100 py-2">
             <div class="card-body">
@@ -97,7 +97,7 @@ $mydb->setQuery($query3);
 $cur3 = $mydb->loadResultList();
 foreach ($cur3 as $result3) {
 ?>
-<div class="col-xl-2 col-md-2 mb-4">
+<div class="col-xl-3 col-md-2 mb-4">
     <div class="card board1 fill">
         <div class="card shadow h-100 py-2">
             <div class="card-body">
@@ -124,7 +124,7 @@ $mydb->setQuery($query4);
 $cur4 = $mydb->loadResultList();
 foreach ($cur4 as $result4) {
 ?>
-<div class="col-xl-2 col-md-2 mb-4">
+<div class="col-xl-3 col-md-2 mb-4">
     <div class="card board1 fill">
         <div class="card shadow h-100 py-2">
             <div class="card-body">
@@ -151,7 +151,7 @@ $mydb->setQuery($query5);
 $cur5 = $mydb->loadResultList();
 foreach ($cur5 as $result5) {
 ?>
-<div class="col-xl-2 col-md-2 mb-4">
+<div class="col-xl-3 col-md-2 mb-4">
     <div class="card board1 fill">
         <div class="card shadow h-100 py-2">
             <div class="card-body">
@@ -178,7 +178,7 @@ $mydb->setQuery($query6);
 $cur6 = $mydb->loadResultList();
 foreach ($cur6 as $result6) {
 ?>
-<div class="col-xl-2 col-md-2 mb-4">
+<div class="col-xl-3 col-md-2 mb-4">
     <div class="card board1 fill">
         <div class="card shadow h-100 py-2">
             <div class="card-body">
