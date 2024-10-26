ca5c43c209d18b0eeccaa4994ee98417c7827608
capstonehotel <ungonkathleen@gmail.com>
2024-10-26 02:38:26 -0500

    v

--- admin/themes/backendTemplate.php
+++ admin/themes/backendTemplate.php
@@ -185,7 +185,7 @@ try {
             <div id="content">
 
                 <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top ">
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
