8c8397e1752b8dacf1a417bcfa18eeaaf25b77ad
capstonehotel <ungonkathleen@gmail.com>
2024-10-26 02:05:19 -0500

    s

--- admin/home.php
+++ admin/home.php
@@ -261,8 +261,8 @@ foreach ($cur7 as $result7) {
 
 
 
-<div class="row mt-4">
-<div class="col-xl-8 col-lg-7">
+
+<div class="col-xl-8 col-lg-7 mb-4">
     <div class="card shadow mb-4">
         <!-- Card Header - Dropdown -->
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -276,7 +276,7 @@ foreach ($cur7 as $result7) {
         </div>
     </div>
 </div>
-</div>
+
 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
