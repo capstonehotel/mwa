4fceab313adefd63a4db652ed26942e4a46365b7
capstonehotel <ungonkathleen@gmail.com>
2024-10-26 17:49:08 -0500

    s

--- admin/home.php
+++ admin/home.php
@@ -313,30 +313,32 @@ for ($year = $startYear; $year <= $currentYear; $year++) {
 ?>
 
 
-<div class="col-md-12 col-lg-6">
-<div class="card shadow mb-4">
-    <div class="card card-chart">
-        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 class="card-titlem-0 font-weight-bold text-primary">LINE CHART</h6>
-        </div>
-        <div class="card-body">
-            <div id="line-chart" style="height: 300px;"></div>
-        </div>
-    </div>
-</div>
-</div>
-<div class="col-md-12 col-lg-6">
-<div class="card shadow mb-4">
-    <div class="card card-chart">
-        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 class="card-title m-0 font-weight-bold text-primary">DONUT CHART</h6>
-        </div>
-        <div class="card-body">
-            <div id="donut-chart" style="height: 300px;"></div>
+<div class="col-md-6">
+    <div class="card shadow mb-4" style="height: 400px;"> <!-- Set equal height -->
+        <div class="card card-chart">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="card-title m-0 font-weight-bold text-primary">LINE CHART</h6>
+            </div>
+            <div class="card-body">
+                <div id="line-chart" style="height: 300px; width: 100%;"></div>
+            </div>
         </div>
     </div>
 </div>
+
+<div class="col-md-6">
+    <div class="card shadow mb-4" style="height: 400px;"> <!-- Set equal height -->
+        <div class="card card-chart">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="card-title m-0 font-weight-bold text-primary">DONUT CHART</h6>
+            </div>
+            <div class="card-body">
+                <div id="donut-chart" style="height: 300px; width: 100%;"></div>
+            </div>
+        </div>
+    </div>
 </div>
+
 
 <script>
 $(document).ready(function() {
