4866f5ea863bc89fe7e73e4e7b3bbc1203978d87
capstonehotel <ungonkathleen@gmail.com>
2024-10-25 23:07:22 -0500

    s

--- admin/home.php
+++ admin/home.php
@@ -17,7 +17,7 @@ foreach ($cur as $result) {
                 <div class="dash-widget-header">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="h5 mb-0 font-weight-bold text-gray-800" style="font-size: 30px; color: #b90000;"><?php echo isset($result->Total) ? $result->Total : 0; ?></div>
+                            <div class="h5 mb-0 font-weight-bold " style="font-size: 30px; color: #b90000;"><?php echo isset($result->Total) ? $result->Total : 0; ?></div>
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Rooms</div>
                         </div>
                         <div class="col-auto">
