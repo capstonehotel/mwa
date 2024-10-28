39ad05dc5f4a22bf5183be30e6be2ef2e2bd0a7d
capstonehotel <ungonkathleen@gmail.com>
2024-10-28 09:58:45 -0500

    b

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 // Assuming you already have a connection to the database
 // $conn = new mysqli("localhost", "username", "password", "database");
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 
 $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
