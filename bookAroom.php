e404f5c684086c51a9481d750a02262d5db58172
capstonehotel <ungonkathleen@gmail.com>
2024-10-27 23:23:54 -0500

    s

--- bookAroom.php
+++ bookAroom.php
@@ -610,7 +610,14 @@ if ($countrated == 0) {
 </div>
 </div>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@panzoom/panzoom@4.5.1/dist/panzoom.min.js"></script>
 <script>
 $(document).ready(function () {
 $('[id^="roomModal"]').on('shown.bs.modal', function () {
