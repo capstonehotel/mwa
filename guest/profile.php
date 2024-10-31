60258ca53bea9a76d46a57f0dac93e969575d78e
capstonehotel <ungonkathleen@gmail.com>
2024-10-30 23:19:40 -0500

    v

--- guest/profile.php
+++ guest/profile.php
@@ -27,21 +27,7 @@
 <body>
 <?php 
 require_once("../includes/initialize.php");
-// load config file first 
-// require_once("../includes/config.php");
-// //load basic functions next so that everything after can use them
-// require_once("../includes/functions.php");
-// //later here where we are going to put our class session
-// require_once("../includes/session.php");
-// require_once("../includes/user.php");
-// require_once("../includes/pagination.php");
-// require_once("../includes/paginsubject.php");
-// require_once("../includes/accomodation.php");
-// require_once("../includes/guest.php");
-// require_once("../includes/reserve.php"); 
-// require_once("../includes/setting.php");
-// //Load Core objects
-// require_once("../includes/database.php");
+
   
 $guest = New Guest();
 $res = $guest->single_guest($_SESSION['GUESTID']);
