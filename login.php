c2307a1716b837f37ed204d19591f0a6ea1fc8e7
capstonehotel <ungonkathleen@gmail.com>
2024-10-30 22:24:50 -0500

    c

--- login.php
+++ login.php
@@ -17,8 +17,8 @@
         $guest = new Guest();
         $res = $guest::guest_login($email,$h_upass);
  // You need to fetch the guest's first and last name
- $_SESSION['G_FNAME'] = $res['G_FNAME']; // Ensure guest_login returns these values
- $_SESSION['G_LNAME'] = $res['G_LNAME'];
+//  $_SESSION['G_FNAME'] = $res['G_FNAME']; // Ensure guest_login returns these values
+//  $_SESSION['G_LNAME'] = $res['G_LNAME'];
        
         if ($res == true) {
          // Send OTP
