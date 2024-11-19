877391ecf00d09ac00a1e6262e6f1b61116a75c8
capstonehotel <ungonkathleen@gmail.com>
2024-11-19 02:27:10 -0500

    s

--- login.php
+++ login.php
@@ -1,25 +1,32 @@
 <?php
-//user
-require_once("initialize.php"); 
+ require_once ("initialize.php"); 
+ //require_once ("sendOTP.php");
 
-if (isset($_POST['gsubmit'])) { 
-    $email = trim($_POST['username']);
-    $upass = trim($_POST['pass']); // use the plain password
+
+ if(isset($_POST['gsubmit'])){
+
     
-    if ($email == '' OR $upass == '') { 
-        message("Invalid Username and Password!", "error");
-        redirect("https://mcchmhotelreservation.com/index.php?page=6");
+
+  $email = trim($_POST['username']);
+  $upass  = trim($_POST['pass']);
+
+  }
+   if ($email == '' OR $upass == '') { 
+      message("Invalid Username and Password!", "error");
+       redirect("https://mcchmhotelreservation.com/index.php?page=6");
+         
     } else {   
         $guest = new Guest();
-        // Pass plain password instead of hashed password
-        $res = $guest::guest_login($email, $upass);
+        $res = $guest::guest_login($email,$h_upass);
 
         if ($res == true) {
-            redirect("https://mcchmhotelreservation.com/booking/index.php?view=payment");
-        } else {
-            message("Invalid Username and Password! Please contact administrator", "error");
-            redirect("https://mcchmhotelreservation.com/booking/index.php?view=logininfo");
-        }
-    }
-}
+       
+             redirect("https://mcchmhotelreservation.com/booking/index.php?view=payment");
+         } else {
+             message(" Please try again.", "error");
+             redirect("https://mcchmhotelreservation.com/booking/index.php?view=logininfo");
+         }
+     }
+ 
+ 
 ?>
