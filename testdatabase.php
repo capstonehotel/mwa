aada5a48e93530a4ccf2000cb24309c559b4318c
capstonehotel <ungonkathleen@gmail.com>
2024-11-19 17:49:50 -0500

    t

--- testdatabase.php
+++ testdatabase.php
@@ -21,7 +21,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"];
+    echo "id: " . $row["ROOMID"] . "<br />";
   }
 } else {
   echo "0 results";
