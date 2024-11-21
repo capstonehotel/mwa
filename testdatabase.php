ffc0f98544a65be4de1771a109f1d742a2203fc3
capstonehotel <ungonkathleen@gmail.com>
2024-11-21 10:49:46 -0500

    v

--- testdatabase.php
+++ testdatabase.php
@@ -20,7 +20,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo $row["livechat"] . "\n";
+        echo "Tables " . $row["notifications,livechat,tblroom,tblguest"] . "<br />";
     }
 } else {
     echo "No tables found.";
