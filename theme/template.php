c0bc175b656f49322b1f2e8d163394b802f288d3
capstonehotel <ungonkathleen@gmail.com>
2024-11-21 15:00:59 -0500

    s

--- theme/template.php
+++ theme/template.php
@@ -682,7 +682,7 @@ s0.parentNode.insertBefore(s1,s0);
             ?>
 
           
-                    <li class="nav-item dropdown">
+                    <li class="nav-item dropdown" style="right: 50px;">
                       <a class="nav-link active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                          <i class="fa fa-user fa-fw"></i><?php echo $_SESSION['name']. ' ' . $_SESSION['last']; ?> <i class="fa fa-caret-down fa-fw"></i> 
                       </a>
