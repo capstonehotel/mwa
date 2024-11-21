98f8acf3c56ac945921b06785ed306e1b3c266d4
capstonehotel <ungonkathleen@gmail.com>
2024-11-21 13:20:56 -0500

    s

--- admin/themes/chatbox.php
+++ admin/themes/chatbox.php
@@ -5,23 +5,23 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-require_once 'https://mcchmhotelreservation.com/includes/initialize.php';
+// require_once 'https://mcchmhotelreservation.com/includes/initialize.php';
 // Check if the necessary POST variables are set
 
     $user_id = $_GET['user_id'];
     $name = $_GET['name'];
     $message = $_GET['message'];
 
-    // // Database connection details
-    // $servername = "127.0.0.1";
-    // $username = "u510162695_hmsystemdb";
-    // $password = "1Hmsystemdb";
-    // $dbname = "u510162695_hmsystemdb";
-    // $dbport ="3306";
+    // Database connection details
+    $servername = "127.0.0.1";
+    $username = "u510162695_hmsystemdb";
+    $password = "1Hmsystemdb";
+    $dbname = "u510162695_hmsystemdb";
+    $dbport ="3306";
 
     // // Create connection
-    // $conn = new mysqli($servername, $username, $password, $dbname, $dbport);
-    $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+    $conn = new mysqli($servername, $username, $password, $dbname, $dbport);
+    //$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -33,16 +33,8 @@ require_once 'https://mcchmhotelreservation.com/includes/initialize.php';
     } else {
         // Prepare and execute SQL query
         $sql = "INSERT INTO livechat (sender_id, name, message, status) VALUES (?, ?, ?, '0')";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("iss", $user_id, $name, $message);
-
-        if ($stmt->execute()) {
-            echo "Sent";
-        } else {
-            echo "Error: " . $stmt->error;
-        }
-
-        $stmt->close();
+        $conn->query($sql);
+        echo "sent";
     }
 
     $conn->close();
