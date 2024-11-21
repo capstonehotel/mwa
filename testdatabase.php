8f3bce8f47ece7a527bdc50a0f6c5a333d71767e
capstonehotel <ungonkathleen@gmail.com>
2024-11-21 13:27:54 -0500

    t

--- testdatabase.php
+++ testdatabase.php
@@ -20,7 +20,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "ID " . $row["sender_id"] . "<br />";
+        echo "ID " . $row["name"] . "<br />";
     }
 } else {
     echo "No tables found.";
