6a1332d0744d965bd5c729832b4c08a0cdbf4759
capstonehotel <ungonkathleen@gmail.com>
2024-11-21 13:01:25 -0500

    ta

--- admin/themes/chatbox.php
+++ admin/themes/chatbox.php
@@ -1,20 +1,20 @@
 <?php
 // Check if the necessary POST variables are set
-if (isset($_GET['user_id']) && isset($_GET['name']) && isset($_GET['message'])) {
+
     $user_id = $_GET['user_id'];
     $name = $_GET['name'];
     $message = $_GET['message'];
 
-    // // Database connection details
-    // $servername = "127.0.0.1";
-    // $username = "u510162695_hmsystemdb";
-    // $password = "1Hmsystemdb";
-    // $dbname = "u510162695_hmsystemdb";
-    // $dbport ="3306";
+    // Database connection details
+    $servername = "127.0.0.1";
+    $username = "u510162695_hmsystemdb";
+    $password = "1Hmsystemdb";
+    $dbname = "u510162695_hmsystemdb";
+    $dbport ="3306";
 
     // // Create connection
-    // $conn = new mysqli($servername, $username, $password, $dbname, $dbport);
-    $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+    $conn = new mysqli($servername, $username, $password, $dbname, $dbport);
+    // $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -40,5 +40,5 @@ if (isset($_GET['user_id']) && isset($_GET['name']) && isset($_GET['message'])) 
 
     $conn->close();
 
-}
+
 ?>
