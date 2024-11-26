11732f3ff2a300ea6c12de9decde9c2ef0f67c66
capstonehotel <ungonkathleen@gmail.com>
2024-11-26 15:20:24 -0500

    x

--- testdatabase.php
+++ testdatabase.php
@@ -3,33 +3,69 @@ $servername = "127.0.0.1";
 $username = "u510162695_hmsystemdb";
 $password = "1Hmsystemdb";
 $dbname = "u510162695_hmsystemdb";
-$dbport ="3306";
+$dbport = "3306";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname, $dbport);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT * FROM livechat";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "ID " . $row["id"] . "<br />";
-        echo "sender_id " . $row["sender_id"] . "<br />";
-        echo "user_name" . $row["username"] . "<br />";
-        echo "message" . $row["message"] . "<br />";
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Sender ID</th><th>Username</th><th>Message</th></tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($row["id"]) . "</td>";
+        echo "<td>" . htmlspecialchars($row["sender_id"]) . "</td>";
+        echo "<td>" . htmlspecialchars($row["username"]) . "</td>";
+        echo "<td>" . htmlspecialchars($row["message"]) . "</td>";
+        echo "</tr>";
     }
+    echo "</table>";
 } else {
-    echo "No tables found.";
+    echo "No records found.";
 }
 
 $conn->close();
+
+
+// $servername = "127.0.0.1";
+// $username = "u510162695_hmsystemdb";
+// $password = "1Hmsystemdb";
+// $dbname = "u510162695_hmsystemdb";
+// $dbport ="3306";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname, $dbport);
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+// $sql = "SELECT * FROM livechat";
+// $result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+//     while($row = $result->fetch_assoc()) {
+//         echo "ID " . $row["id"] . "<br />";
+//         echo "sender_id " . $row["sender_id"] . "<br />";
+//         echo "user_name" . $row["username"] . "<br />";
+//         echo "message" . $row["message"] . "<br />";
+//     }
+// } else {
+//     echo "No tables found.";
+// }
+
+// $conn->close();
 // $sql = "SELECT * FROM notifications";
 // $result = $conn->query($sql);
 
