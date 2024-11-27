3ed7b8b553f1618d11aed9017a7c9dabf0a3cabe
capstonehotel <ungonkathleen@gmail.com>
2024-11-26 22:04:28 -0500

    n

--- admin/themes/chatbox.php
+++ admin/themes/chatbox.php
@@ -1,4 +1,44 @@
 <?php
+// require_once ("../../initialize.php");
+// // Check if the necessary POST variables are set
+// if (isset($_POST['user_id']) && isset($_POST['user_name']) && isset($_POST['message'])) {
+//     $user_id = $_POST['user_id'];
+//     $name = $_POST['user_name'];
+//     $message = $_POST['message'];
+
+   
+
+//     // Create connection
+//     $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
+//     // Check connection
+//     if ($conn->connect_error) {
+//         die("Connection failed: " . $conn->connect_error);
+//     }
+
+//     // Check if message is empty
+//     if (empty($message)) {
+//         echo "error";
+//     } else {
+//         // Prepare and execute SQL query
+//         $sql = "INSERT INTO livechat (sender_id, user_name, message, status) VALUES (?, ?, ?, '0')";
+//         $stmt = $conn->prepare($sql);
+//         $stmt->bind_param("iss", $user_id, $name, $message);
+
+//         if ($stmt->execute()) {
+//             echo "Sent";
+//         } else {
+//             echo "Error: " . $stmt->error;
+//         }
+
+//         $stmt->close();
+//     }
+
+//     $conn->close();
+
+// }
+?>
+<?php
 require_once("../../initialize.php");
 
 // Check if the necessary POST variables are set
