073a09092cb6a50e77396ee0082f966cff834f11
capstonehotel <ungonkathleen@gmail.com>
2024-11-28 00:34:54 -0500

    n

--- booking/personalinfo.php
+++ booking/personalinfo.php
@@ -528,6 +528,7 @@ document.addEventListener('DOMContentLoaded', function() {
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
 
