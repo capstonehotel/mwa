682a4612e5647bd52e69a55517d3451fff7519b0
capstonehotel <ungonkathleen@gmail.com>
2024-12-01 23:22:49 -0500

    v

--- admin/mod_payment/controller.php
+++ admin/mod_payment/controller.php
@@ -1,5 +1,5 @@
 <?php
-$connection = new mysqli('localhost', 'root', '', 'hmsystemdb');
+ $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 require_once("../../includes/initialize.php");
 require_once("../../includes/config.php");
 echo '<script src="../sweetalert2.all.min.js"></script>';
