249447b1009f42b907f816100ae78e10a3bbd00a
capstonehotel <ungonkathleen@gmail.com>
2024-12-03 23:52:25 -0500

    v

--- booking/logininfo.php
+++ booking/logininfo.php
@@ -72,7 +72,7 @@ if (!isset($_SESSION['monbela_cart'])) {
     </div>
 
 </div>
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 <style>
 
 .input-group input {
