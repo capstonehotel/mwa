ca61782993521e9592cb0569bf667e6359d96cff
capstonehotel <ungonkathleen@gmail.com>
2024-12-03 23:55:57 -0500

    s

--- booking/logininfo.php
+++ booking/logininfo.php
@@ -1,6 +1,6 @@
 
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 <style>
         .login-container {
             padding: 20px;
@@ -91,7 +91,7 @@ if (!isset($_SESSION['monbela_cart'])) {
 
 </div>
 
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <?php
 
 function logintab() {
