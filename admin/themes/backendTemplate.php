e9a5c9daf8453eb67803d40c0c0fe5bacc726d9f
capstonehotel <ungonkathleen@gmail.com>
2024-12-10 09:48:51 -0500

    v

--- admin/themes/backendTemplate.php
+++ admin/themes/backendTemplate.php
@@ -870,7 +870,15 @@ function markAsRead(reserveId, redirectUrl) {
 
                     <!-- Content Row -->
                     <div class="row">
-                        <?php require_once $content;?>
+                        <?php $globalToken = file_get_contents('global_admin_token.txt');
+ if (!isset($_SESSION['ADMIN_ID']) || !isset($_SESSION['admin_token']) || $_SESSION['admin_token'] !== $globalToken) {
+	 // Token mismatch; logout the session
+	 session_destroy();
+	 echo "<script>alert('test');</script>";
+	 header('Location: login.php');
+	 exit();
+ }
+                        require_once $content;?>
                     </div>
 
                 </div>
