982040b6e8de3de750a4b4ed4123b2daa9ec7e4e
capstonehotel <ungonkathleen@gmail.com>
2024-12-12 15:46:43 -0500

    s

--- booking/terms_condition.php
+++ booking/terms_condition.php
@@ -12,50 +12,56 @@
   <p>
 TERMS & CONDITIONS
 <ul>
-<h4>Reservations</h4>
-Through our online booking system, you will be able to check room availability and make your booking. For peace of mind, you will receive by email a reservation confirmation, including the details of your reservation. It is recommended that you keep a copy of this reservation confirmation, preferably by printing the page. If you have to modify or cancel your booking, please contact our reservation office as specified in your reservation confirmation. Also, we do not accept a booking exceeding 1 year reservation prior to the arrival date.
+< <h4>Reservations</h4>
+                        <ul>
+                            <li>Through our online booking system, you will be able to check room availability and make your booking. For peace of mind, you will receive by email a reservation confirmation, including the details of your reservation. It is recommended that you keep a copy of this reservation confirmation, preferably by printing the page.</li>
+                            <li>If you have to modify or cancel your booking, please contact our reservation office as specified in your reservation confirmation. Also, we do not accept a booking exceeding 1 year reservation prior to the arrival date.</li>
+                        </ul>
 
-<h4>Cancellation </h4>
-● If there is a cancellation of a room reservation you can canceled it, but it is non-refundable. The payments cannot be refunded once you have already booked for the reservation. 
+                        <h4>Cancellation</h4>
+                        <ul>
+                            <li>If there is a cancellation of a room reservation, you can cancel it, but it is non-refundable. The payments cannot be refunded once you have already booked for the reservation.</li>
+                            <li>Additional Requests: All additional or special requests are subject to availability, and we cannot guarantee the provision for special requests. Any additional requests made should be made prior to your arrival at the hotel, giving reasonable advance notice.</li>
+                        </ul>
 
-● Additional Requests 
+                        <h4>General Information</h4>
+                        <ul>
+                            <li><strong>Accommodation:</strong> As a minimum, all bedrooms feature a private ensuite bathroom with either a bath & shower or a shower and a color television.</li>
+                            <li><strong>Opening hours:</strong> Our reception is open 24 hours. If you wish to check in to the hotel out of these hours, please discuss with reception at the time of booking.</li>
+                            <li><strong>Payment Methods:</strong> We accept most major credit and debit cards, cash, and online payment through GCash.</li>
+                        </ul>
 
-All additional or special requests are subject to availability, and we cannot guarantee the provision for special requests. Any additional requests made should be made prior to your arrival at the hotel, giving reasonable advance notice. 
- 
+ <h4>Price Guarantee</h4>
+                        <ul>
+                            <li>On receipt of written confirmation, the prices quoted and confirmed in writing by the Hotel remain fixed except for any alterations in the Government rates of taxation and/or duty such as VAT, for which we reserve the right to alter pricing to take account of any variation.</li>
+                        </ul>
 
-<h4>General Information  </h4>
-<li>Accommodation: As a minimum, all bedrooms feature a private ensuite bathroom with either a bath & shower or a shower and color television. 
+                        <h4>Making a Booking</h4>
+                        <ul>
+                            <li>By making a booking, you are confirming that you are authorized to do so on behalf of all people named in the booking, and you are acknowledging that all members of your party agree to be bound by these Booking Terms & Conditions.</li>
+                            <li>When your booking has been made, confirmation can be sent to you by email using the email address that you have supplied, or by post. Booking confirmations are subject to the availability of accommodation at the hotel.</li>
+                            <li>You should carefully check the details of your confirmation as soon as you receive it. You must contact the hotel immediately if any of the details are incorrect or incomplete.</li>
+                            <li>We will always endeavor to rectify any inaccuracies or accommodate any alterations you wish to make to your booking. We cannot accept liability for any inaccuracies that are not brought to our attention within seven days of issuing your confirmation, nor can we accept responsibility for inaccurate information that you have supplied.</li>
+                        </ul>
 
-Opening hours: Our reception is open 24hours. If you wish to check-in to the hotel out of these hours, please discuss reception at the time of booking. 
+                        <h4>Paying for Your Booking</h4>
+                        <ul>
+                            <li>Credit or debit card details will be required when you make your booking. You can choose to pay the full payment or half payment of your booking. In some instances, a non-refundable deposit or full payment may be required at the time of booking.</li>
+                            <li>Unless stated as part of your booking, additional items such as (but not limited to) the cost of external telephone calls, are not included in the price of your stay. If you incur any such additional costs, you must settle the sum involved on your departure from the hotel.</li>
+                        </ul>
 
-Payment Methods: We accept most major credit and debit cards, cash, and online payment through gcash. 
+                        <h4>Circumstances Beyond Our Control</h4>
+                        <ul>
+                            <li>We cannot accept responsibility for unforeseen circumstances beyond our control. These include (but are not limited to) adverse weather conditions, fire, riot, war, terrorist activity (or threat of such activity), industrial dispute, natural disaster, or injuries and death of an individual(s) through accidental circumstances unconnected with the hotel.</li>
+                            <li>By making a booking, you are accepting responsibility for any damage or loss caused by yourself or a member of your party. Full payment for any such damage or loss must be paid to the hotel owner or manager on demand. If you fail to do so, you will be responsible for meeting any claims subsequently made (together with our own and the other party's full legal costs) as a result of your actions.</li>
+                        </ul>
+                        <h4>Complaints</h4>
+                        <ul>
+                            <li>If you are dissatisfied with any aspect of your stay, you should bring the problem or issue to the attention of the duty manager or senior member of staff at the hotel as soon as possible so that all reasonable efforts can be made to rectify the situation.</li>
+                            <li>If, for any reason, the issue cannot be resolved to your satisfaction and you wish to make a complaint, you should put it in writing and send it to the owner. If you have any questions, please message using the chatbox or message through our Gmail account: <a href="mailto:mcchmhotelreservation@gmail.com">mcchmhotelreservation@gmail.com</a>.</li>
+                        </ul>
 
-<h4>Price Guarantee</h4>
-<li>On receipt of written confirmation, the prices quoted and confirmed in writing by the Hotel remain fixed except for any alterations in the Government rates of taxation and/or duty such as VAT, for which we reserve the right to alter pricing to take account of any variation. 
-<h4>Paying for your booking</h4>
-Credit or debit card details will be required when you make your booking. No money will actually be taken although your card will be verified at the time of booking for 100% of your first nights stay. See our cancellation policy for charges that may be taken. In some instances, a non-refundable deposit or full payment may be required at the time of booking.
-Unless stated as part of your booking, additional items such as (but not limited to) the cost of external telephone calls, are not included in the price of your stay. If you incur any such additional costs you must settle the sum involved on your departure from the hotel.
-
-<h4>Making a booking </h4>
-<li>By making a booking you are confirming that you are authorized to do so on behalf of all people named in the booking and you are acknowledging that all members of your party agree to be bound by these Booking Terms & Conditions. 
-
-When your booking has been made confirmation can be sent to you by email using the email address that you have supplied, or by post. Booking confirmations are subject to the availability of accommodation at the hotel. 
-
-You should carefully check the details of your confirmation as soon as you receive it. You must contact the hotel immediately if any of the details are incorrect or incomplete. 
-
-We will always endeavor to rectify any inaccuracies or accommodate any alterations you wish to make to your booking. We cannot accept liability for any inaccuracies that are not brought to our attention within seven days of issuing your confirmation, nor can we accept responsibility for inaccurate information that you have supplied. 
-
-
-<h4>Circumstances beyond our control  </h4>
-We cannot accept responsibility for unforeseen circumstances beyond our control. These include (but are not limited to) adverse weather conditions, fire, riot, war, terrorist activity (or threat of such activity), industrial dispute, natural disaster, or injuries and death of an individual(s) through accidental circumstances unconnected with the hotel. 
-
-Complaints 
-
-If you are dissatisfied with any aspect of your stay, you should bring the problem or issue to the attention of the duty manager or senior member of staff at the hotel as soon as possible so that all reasonable efforts can be made to rectify the situation. If, for any reason, the issue cannot be resolved to your satisfaction and you wish to make a complaint, you should put it in writing and send it to the owner. If you have any questions, please message using the chatbox or message through our gmail account; mcchmhotelreservation@gmail.com .  
-
-Thank you Respectfully yours, HM Mini Hotel 
-
- 
+                        <p>Thank you. Respectfully yours, <br><strong>HM Mini Hotel</strong></p>
 </ul> 
 </p>
 </div>
