19e36aecd911af0aff7b48a10f74ed595840c628
capstonehotel <ungonkathleen@gmail.com>
2024-12-12 14:09:33 -0500

    v

--- about_us.php
+++ about_us.php
@@ -17,7 +17,7 @@
               <div class="space"></div>
               <p><i class="fa fa-map-marker fa-fw pull-left fa-2x"></i><span class="link-text" onclick="window.location.href='https://www.google.com/maps/place/Madridejos+Community+College/@11.2636504,123.7209869,17z/data=!3m1!4b1!4m6!3m5!1s0x33a88140310a21a9:0xc5b9b94e9c2702db!8m2!3d11.2636451!4d123.7235618!16s%2Fg%2F1hc28c7s2?entry=ttu';">Crossing Bunakan Madridejos Cebu</span></p>
               <div class="space"></div>
-              <p><i class="fa fa-envelope-o fa-fw pull-left fa-2x"></i><span class="link-text" onclick="window.location.href='mailto:Hmhotel@gmail.com';">Hmhotel@gmail.com</span></p>
+              <p><i class="fa fa-envelope-o fa-fw pull-left fa-2x"></i><span class="link-text" onclick="window.location.href='mailto:Hmhotel@gmail.com';">mcchmhotelreservation@gmail.com</span></p>
               <div class="space"></div>
               <p><i class="fa fa-phone fa-fw pull-left fa-2x"></i><span class="link-text" onclick="window.location.href='tel:+1234567890';">09317622381</p>
               <p><i class="fa fa-facebook fa-fw pull-left fa-2x"></i><span class="link-text" onclick="window.location.href='https://web.facebook.com/p/Madridejos-Community-College-Hospitality-Management-Department-100054453806197/?paipv=0&eav=AfYUS3bOnNWDmiBS86XIEqDgKwonPli4DFEY8cjKFQSxZq-ZEAJASAViws-aNONi-rM&_rdc=1&_rdr';">Hospitality Management Department</span></p>
