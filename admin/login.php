bb80af58b5ff8ab42b43288e3bcdca8258c27f38
capstonehotel <ungonkathleen@gmail.com>
2024-12-18 16:05:43 -0500

    s

--- admin/login.php
+++ admin/login.php
@@ -410,9 +410,7 @@ if (isNewDevice($connection, $user, $device, $ip_address)) {
   $_SESSION['ADMIN_UPASS'] = $row['UPASS'];
   $_SESSION['ADMIN_UROLE'] = $row['ROLE'];
 
-  $stmt = $connection->prepare("INSERT INTO sessions (user, device, location, ip_address) VALUES (?, ?, ?, ?)");
-                     $stmt->bind_param("ssss", $user, $device, $location, $ip_address);
-                     if ($stmt->execute()) {
+ 
                         echo "<script>
                      Swal.fire({
                          icon: 'success',
@@ -422,7 +420,7 @@ if (isNewDevice($connection, $user, $device, $ip_address)) {
                          window.location.href = 'index';
                      });
                    </script>";
-                     } 
+                    
 
                      
  
