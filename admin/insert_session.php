9adfb42180b5fc1c5b59a77efa7ad9cc43a905a1
capstonehotel <ungonkathleen@gmail.com>
2024-12-18 14:53:18 -0500

    s

--- admin/insert_session.php
+++ admin/insert_session.php
@@ -1,5 +1,6 @@
 <?php
 require_once("../includes/initialize.php");
+
 // Receive the session data from the AJAX request
 if (isset($_POST['user'], $_POST['device'], $_POST['location'], $_POST['ip_address'])) {
     $user = $_POST['user'];
@@ -9,13 +10,23 @@ if (isset($_POST['user'], $_POST['device'], $_POST['location'], $_POST['ip_addre
 
     // Prepare the session insertion query
     $stmt = $connection->prepare("INSERT INTO sessions (user, device, location, ip_address) VALUES (?, ?, ?, ?)");
+    
+    // Check for errors in the preparation step
+    if (!$stmt) {
+        echo 'Error preparing statement: ' . $connection->error;
+        exit();
+    }
+
+    // Bind the parameters to the statement
     $stmt->bind_param("ssss", $user, $device, $location, $ip_address);
 
     // Execute the query
     if ($stmt->execute()) {
         echo 'success'; // If session is inserted successfully
     } else {
-        echo 'error'; // If there was an error with the insertion
+        echo 'Error executing query: ' . $stmt->error; // If there was an error with the insertion
     }
+} else {
+    echo 'Missing required parameters'; // If any required POST data is missing
 }
 ?>
