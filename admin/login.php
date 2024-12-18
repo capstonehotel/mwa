8f3857fd8061321cebaa58208ba5bd91099976dc
capstonehotel <ungonkathleen@gmail.com>
2024-12-18 15:04:30 -0500

    c

--- admin/login.php
+++ admin/login.php
@@ -379,7 +379,7 @@ if (isNewDevice($connection, $user, $device, $ip_address)) {
                                             window.location = 'index';
                                         });
                                     } else {
-                                        Swal.fire('Error', 'insertResponse', 'error');
+                                        Swal.fire('Error', insertResponse, 'error');
                                     }
                                 });
                                                  }
