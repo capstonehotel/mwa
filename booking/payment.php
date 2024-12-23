fb446d0e26b5a714491cb32b26a69df02db5a10e
capstonehotel <ungonkathleen@gmail.com>
2024-12-23 00:52:02 -0500

    v

--- booking/payment.php
+++ booking/payment.php
@@ -348,27 +348,31 @@ if (!$msg1) {
     <div class="container">
         <div class="row">
             <form action="index?view=payment" method="post" name="personal" enctype="multipart/form-data" id="bookingForm">
-                <div class="col-md-8 col-sm-4">
-                    <div class="col-md-12 form-group d-flex align-items-center" >
-                        <label style="margin-right: 10px;">Name:</label>
-                        <?php echo $_SESSION['name'] . ' ' . $_SESSION['last']; echo $count_cart; ?>
-                    </div>
-                    <div class="col-md-12 form-group d-flex align-items-center">
+                
+                    <div class="row mb-4">
+                <div class="col-md-8">
+                <div class="form-group d-flex align-items-center">
+            <label style="margin-right: 10px;">Name:</label>
+            <p class="form-control-plaintext" >
+                <?php echo $_SESSION['name'] . ' ' . $_SESSION['last'];  echo $count_cart; ?>
+            </p>
+        </div>
+                    <div class="form-group d-flex align-items-center">
                         <label style="margin-right: 10px;">Address:</label>
                         <?php echo isset($_SESSION['city']) ? $_SESSION['city'] : ' ' . ' ' . (isset($_SESSION['address']) ? $_SESSION['address'] : ' '); ?> 
                     </div>
-                    <div class="col-md-12 form-group d-flex align-items-center">
+                    <div class=" form-group d-flex align-items-center">
                         <label style="margin-right: 10px;">Phone #:</label>
                         <?php echo $_SESSION['phone']; ?>
                     </div>
                 </div>
                 
-                    <div class="col-md-12 form-group d-flex align-items-center">
+                    <div class=" form-group d-flex align-items-center">
                         <label style="margin-right: 10px;">Transaction Date:</label>
                         <?php echo date("m/d/Y"); ?>
                     </div>
                     
-                    <div class="col-md-12 form-group d-flex align-items-center">
+                    <div class=" form-group d-flex align-items-center">
     <label style="margin-right: 10px;" >Transaction Id:</label>
     <span   name="realconfirmation"><?php echo $_SESSION['confirmation']; ?></span>
     <input type="hidden" name="realconfirmation" value="<?php echo $_SESSION['confirmation']; ?>" />
