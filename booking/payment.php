5375571fa415a94003716f405a6797bba62d6ce4
capstonehotel <ungonkathleen@gmail.com>
2024-12-23 00:49:11 -0500

    s

--- booking/payment.php
+++ booking/payment.php
@@ -388,7 +388,7 @@ if (!$msg1) {
         <div>
             <input type="radio" id="gcash" name="payment_method" value="gcash" required>
             <label for="gcash">
-                <img src="../gcash.png" alt="Pay with GCash" style="height: 20px; margin-right: 5px;">
+                <img src="../GCashlogo.png" alt="Pay with GCash" style="height: 20px; margin-right: 5px;">
                 Pay with GCash
             </label>
         </div>
